b38a94fab2167de3cc072b6efb9c551196bdb31e
napatswift <napat.do@ku.th>
2022-09-04 20:24:50 +0700

    Add classes and views

--- src/main/java/th/ac/ku/restaurant/model/User.java
+++ src/main/java/th/ac/ku/restaurant/model/User.java
@@ -8,6 +8,26 @@ import java.util.UUID;
 @Entity
 public class User {
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
     @Id
     @GeneratedValue
     private UUID id;
@@ -22,39 +42,19 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getRole() {
         return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 }
