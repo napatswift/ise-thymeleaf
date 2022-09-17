ae94b47033fa16647ab79f8cb8910be3f6e674fc
napatswift <napat.do@ku.th>
2022-09-17 17:43:43 +0700

    "Add menu" view

--- src/main/java/th/ac/ku/restaurant/config/ComponentConfig.java
+++ src/main/java/th/ac/ku/restaurant/config/ComponentConfig.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ComponentConfig {
@@ -11,5 +12,10 @@ public class ComponentConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
